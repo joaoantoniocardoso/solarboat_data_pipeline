b9c2378e595a0176b9c4351da77fcd968e028966
João Antônio Cardoso <joao.maker@gmail.com>
2023-03-31 23:12:56 -0300

    Add unittest for units from canparser generator

---
+++ tests/test_canparser_generator.py
@@ -0,0 +1,19 @@
+from canparser_generator import CanTopicParser
+from math import isclose
+
+
+def test_apply_units():
+    # Test case 1: No units, no scaling
+    units, value = CanTopicParser.apply_units("", 42)
+    assert units == ""
+    assert isclose(value, 42)
+
+    # Test case 2: Percentage units, scaling
+    units, value = CanTopicParser.apply_units("%", 128)
+    assert units == "%"
+    assert isclose(value, 0.5, rel_tol=0.01)
+
+    # Test case 3: Custom units, scaling
+    units, value = CanTopicParser.apply_units("A/100", 1500)
+    assert units == "A"
+    assert isclose(value, 15.0, rel_tol=0.01)
